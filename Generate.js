7a475f8a8d27ceb6be6c5ad24b88a18d14cf4b35
Deepak Dey <deepakdey593@gmail.com>
2022-08-10 01:46:52 +0530

    Adding JS challenge 2 file Cat Generator

---
+++ Generate.js
@@ -0,0 +1,6 @@
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "f70f751e4817f01a65f80f807ee64a4e.jpg";
+    div.appendChild(image);
+}
